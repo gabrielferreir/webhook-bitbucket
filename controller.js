8835cb663a5057e01af51a59ea47e4765451f8e1
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-07-26 08:06:00 -0300

    Return error

--- controller.js
+++ controller.js
@@ -3,8 +3,8 @@ const exec = require('child_process').exec;
 function web(req, res) {
     let command = `cd ../${req.body.repository.name} && git pull`;
     exec(command, (err, out) => {
-        if (err) console.error(err);
-        return res.status(200).json({content: out})
+        if (err) return res.status(500).json({error: err});
+        return res.status(200).json({content: out});
     });
 }
 
