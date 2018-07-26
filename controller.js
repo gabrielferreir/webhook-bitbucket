2d8fe7b3791383935936c9d99225bce515a87e05
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-07-26 09:02:52 -0300

    --

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').exec;
 
 function web(req, res) {
-    let command = `git pull && docker-compose restart ${req.body.repository.name}`;
+    let command = `git pull && exit && docker-compose restart ${req.body.repository.name}`;
     exec(command, (err, out) => {
         if (err) return res.status(500).json({error: err});
         return res.status(200).json({content: out});
