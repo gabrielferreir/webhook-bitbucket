dd8e13ac01f20dd33bcdc6567d28a2c56b507583
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-07-26 08:12:15 -0300

    git pull

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').exec;
 
 function web(req, res) {
-    let command = `pwd && ls`;
+    let command = `git pull`;
     exec(command, (err, out) => {
         if (err) return res.status(500).json({error: err});
         return res.status(200).json({content: out});
