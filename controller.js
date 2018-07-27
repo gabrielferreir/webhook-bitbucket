65f2a1b516884b2568280bdf2e09c1e45975a3b9
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-07-27 14:45:07 -0300

    Resetando o container

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').exec;
 
 function web(req, res) {
-    let command = `cd ../${req.body.repository.name} && git pull`;
+    let command = `cd ../${req.body.repository.name} && git pull && docker-compose ${req.body.repository.name}`;
     exec(command, (err, out) => {
         if (err) return res.status(500).json({error: err});
         return res.status(200).json({content: out});
